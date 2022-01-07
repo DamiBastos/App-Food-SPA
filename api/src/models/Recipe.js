6093fcb526f19d2b8fbf0590ccf944debbbdc653
DamBastos <damian.bastos@yahoo.com>
2022-01-06 22:47:43 -0300

    First commit

---
+++ api/src/models/Recipe.js
@@ -0,0 +1,41 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('recipe', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV1,
+      allowNull: false,
+      primaryKey: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    summary: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    agregateLikes: {
+      type: DataTypes.INTEGER,
+    },
+    healthScore: {
+      type: DataTypes.INTEGER,
+    },
+    analyzedInstructions: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+      image: {
+        type: DataTypes.STRING
+    
+    },
+    createdInDb: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+      allowNull: false,
+    },
+  });
+};
